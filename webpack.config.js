68d9e403f03b0f51a36ed82a6b62a21d27a0d394
dakulov <akuloc.d.g@gmail.com>
2017-09-30 21:57:15 +0300

    Init project

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'public')
-  }
-};
+  },
+
+  devtool: 'source-map',
+
+  watch: true,
+
+  watchOptions: {
+    poll: 1000,
+    ignored: /node_modules/
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true, sourceMap: true})
+  ]
+}
